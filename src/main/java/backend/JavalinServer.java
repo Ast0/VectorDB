0e103201e64f0f56a8902c8852241ccd569c8f18
Lenard Ehrmuth <ehrmuth@stud.uni-hannover.de>
2023-07-16 04:59:22 +0200

    Created JavalinServer

--- src/main/java/backend/JavalinServer.java
+++ src/main/java/backend/JavalinServer.java
@@ -1,5 +1,47 @@
 package backend;
 
-public class JavalinServer {
-    
+import java.util.Map;
+import java.util.function.Consumer;
+
+import io.javalin.Javalin;
+import io.javalin.websocket.WsConfig;
+import io.javalin.websocket.WsContext;
+
+public class JavalinServer
+{
+    private Map<WsContext, JSONClient> clientMap;
+
+    public void start()
+    {
+        Javalin app = Javalin.create(config -> {
+        }).start();
+
+        app.ws("/chat", ws -> ConfigureWs(ws, false));
+        app.ws("/game", ws -> ConfigureWs(ws, true));
+
+    }
+
+    public static void main(String[] args)
+    {
+        new JavalinServer().start();
+    }
+
+    private void ConfigureWs(WsConfig ws, boolean useGameClient)
+    {
+        if (useGameClient)
+        {
+            ws.onConnect(ctx -> {
+                clientMap.put(ctx, new GameClient(message -> ctx.send(message)));
+            });
+        }
+        else
+        {
+            ws.onConnect(ctx -> {
+                clientMap.put(ctx, new ChatClient(message -> ctx.send(message)));
+            });
+        }
+
+        ws.onClose(ctx -> clientMap.remove(ctx));
+        ws.onMessage(ctx -> clientMap.get(ctx).ReceiveMessage(ctx.message()));
+    }
 }
